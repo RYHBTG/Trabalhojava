1faa6de258b1db9b4b0f97a88744f509658645a0
Héberton <125528084+RYHBTG@users.noreply.github.com>
2023-09-13 19:58:50 -0300

    Update Aquatico.java

--- trabalho/Aquatico.java
+++ trabalho/Aquatico.java
@@ -4,7 +4,9 @@ public class Aquatico extends Animais{
     public Aquatico(String especies, String nomedoanimal, String region, String specialSet) {
         super(especies, nomedoanimal, region, specialSet);
     }
-
+    public Aquatico(){
+        
+    }
     @Override
     public String setEspecies(String especies) {
         super.setEspecies(especies);
