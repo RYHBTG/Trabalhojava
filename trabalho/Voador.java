e8e33d458a95db118d50bc326a59bdf31cc9d1cf
Héberton <125528084+RYHBTG@users.noreply.github.com>
2023-09-13 20:17:00 -0300

    Update Voador.java

--- trabalho/Voador.java
+++ trabalho/Voador.java
@@ -1,17 +1,24 @@
 package trabalho;
 
 public class Voador extends Animais{
-
-    public Voador(String especies, String nomedoanimal, String region, String specialSet) {
+    private String especie;
+    public Voador(String especies, String nomedoanimal, String region, String specialset) {
         super(especies, nomedoanimal, region, specialSet);
     }
     public Voador (){
         
     }
 
-    @Override
-    public String setEspecies(String especies) {
-        super.setEspecies(especies);
-        return especies;
+     @Override
+    public void setEspecies() {
+        super.setEspecies();
+        especie = "Voador";
     }
+    @Override
+    public String mostrarEspecie(){
+        String resumo;
+        resumo = supper.mostrarEspecie();
+        resumo += "\nEspecie: " + getEspecie();
+        return resumo;
+     }
 }
